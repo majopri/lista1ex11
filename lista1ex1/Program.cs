d336f0998288d8df2b7d6a1791a94db731a13b8a
User <User@DESKTOP-G0A3IIV>
2024-04-22 20:08:48 -0300

    Adicionar arquivos de projeto.

---
+++ lista1ex1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista1ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int b;
+            int a;
+            int area;
+
+            Console.Write("digite a base do retangulo;");
+            b = int .Parse(Console.ReadLine());
+
+            Console.Write("digite a altura do retangulo;");
+            a = int. Parse(Console.ReadLine());
+
+            area = b * a;
+
+            Console.Write("area do retangulo é ");
+            Console.WriteLine(area);
+
+        }
+    }
+}
